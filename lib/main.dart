2be8195097731a36d6a205ff64f4989f69f9f5c5
dhairyaCoder <dhairya.coder@gmail.com>
2021-12-23 11:50:09 +0530

    82 Installing External packages and formating dates
    Installing packages -
    1. go onto pub.dev and search the packages
    2. for example search 'intl' package and copy 'intl ^0.0.0' and put it in pubspec.yaml
    3. Save the file. Vscode will automatically load dependencies on saving the file.
    
    Formating dates-
    1. intl is date formating package.
    2. import 'package:intl/intl.dart'
    3. DateFormat('yyyy-MM-dd').dateObject

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import './transaction.dart';
 
@@ -48,11 +49,55 @@ class MyHomePage extends StatelessWidget {
                 color: Colors.blue,
               ),
             ),
-            Card(
-              child: Text('Second Card'),
-              elevation: 5,
-              color: Colors.red,
-            ),
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.purple,
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          '\$' + tx.amount.toString(),
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.purple,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            DateFormat('yyyy-MM-dd').format(tx.date),
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
+            )
           ],
         ));
   }
